54ad29da60f90b3762a26e51a5e6f91464016c8a
Rati <rziraqashvili@gmail.com>
2026-03-31 09:46:20 +0400

    fixed port issues

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(4000, () => {
+  console.log(`Server is running on port 4000}`);
 });
